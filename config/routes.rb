8586ece04ca3e51b43a51fc6f0b05ba1a1cb7261
Elijahscriptdev <elijahobominuru@gmail.com>
2020-04-02 03:17:46 +0100

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
   resources :articles
   resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
