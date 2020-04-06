9a0e201586c021663b40d56ee9dfb4d591352f03
Elijahscriptdev <elijahobominuru@gmail.com>
2020-04-06 11:18:25 +0100

    add validation

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
 
   
   root 'welcome#home'
-  resources :articles
-  resources :categories
+  resources :articles, only: %i[create destroy]
+  resources :categories, only: %i[index show]
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
