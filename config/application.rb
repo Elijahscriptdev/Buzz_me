2e0baf8b6e6978d97597783d619f1134e678026e
Elijahscriptdev <elijahobominuru@gmail.com>
2020-04-12 16:41:22 +0100

    changes

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ Bundler.require(*Rails.groups)
 module StyleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
+    # config.load_defaults 6.0
+    config.autoload = :classic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
