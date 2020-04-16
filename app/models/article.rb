984e75b8d54f9e8ea12deb7854816f7b57948c7b
Elijahscriptdev <elijahobominuru@gmail.com>
2020-04-16 01:14:02 +0100

    updates

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,8 @@ class Article < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :votes
 
+  # acts_as_votable
+
   validates :image, presence: true
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 7, maximum: 9350 }
@@ -11,12 +13,12 @@ class Article < ApplicationRecord
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 
-  scope :first_article, -> { order(created_at: :desc).first(1) }
+  # scope :first_article, -> { order(created_at: :desc).first(1) }
   scope :most_recent, -> { order(created_at: :desc).first(1) }
-  scope :recent, -> { order(created_at: :desc) }
+  # scope :recent, -> { order(created_at: :desc) }
   scope :featured_recent, -> { order(created_at: :desc).first(1) }
-  scope :featured, -> { order(is_upvote: :desc).first(1) }
-  scope :most_popular, -> { order(is_downvote: :desc).first(5) }
+  scope :featured, -> { order(votes_count: :desc).first }
+  scope :most_popular, -> { order(votes_count: :desc).first(5) }
 
   # def upvotes
   #   votes.sum(:upvote)
@@ -29,7 +31,6 @@ class Article < ApplicationRecord
   def upvote
     votes.upvote
   end
-
   def downvote
     votes.downvote
   end
